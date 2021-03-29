b5fcfbb379ce69004bb3f8896230d06a682096ab
sasha <ms.volkova1993@mail.ru>
2021-03-29 23:37:26 +0300

    Задача №2 - Типы данных

---
+++ variable.java
@@ -0,0 +1,10 @@
+package com.max.idea;
+
+public class variable {
+    public static void main(String[] args){
+        double a = -1.235;
+        byte b = 127;
+        System.out.printf("Значение переменной с плавающей точкой: %f \nЗначение переменной целочисленного типа: %d", a, b);
+
+    }
+}
