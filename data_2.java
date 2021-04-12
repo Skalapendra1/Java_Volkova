a26eb79185d2b8e48fd6ec889ba363900e8c69fd
sasha <ms.volkova1993@mail.ru>
2021-04-12 20:22:29 +0300

    Задача №13_ Работа с данными

---
+++ data_2.java
@@ -0,0 +1,20 @@
+package com.max.idea;
+
+import java.util.Scanner;
+
+`public class data_2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String st = scanner.nextLine();
+        String[] r = st.split(" ");
+
+        for (int i = 0; i< r.length; i++) {
+            boolean a = r[i].matches("[a-zA-Z]+");
+            //System.out.print("  " +a);
+            if (a==true){
+            System.out.println("   " +r[i]);
+            }
+        }
+        System.out.println("Количество слов в строке: " +r.length);
+    }
+}
