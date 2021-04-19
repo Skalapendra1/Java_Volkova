5e41f00a662ece271573ead880e8901223c1b271
sasha <ms.volkova1993@mail.ru>
2021-04-19 21:52:24 +0300

    Правки 11 задачи

--- data.java
+++ data.java
@@ -2,21 +2,28 @@ package com.max.idea;
 
 import java.util.Scanner;
 
+import static java.lang.Integer.parseInt;
+
 public class data {
     public static void main(String[] args) {
         Scanner scanner1 = new Scanner(System.in );
         String a1 = scanner1.nextLine();
-        Scanner scanner2 = new Scanner(System.in);
-        int a2 = scanner2.nextInt();
-        int b = Integer.parseInt(a1);
-        if (b>a2){
-            double i = a2;
-            System.out.println("Наибольшее число = "+b);
+        String a2 = scanner1.nextLine();
+
+
+        double b1 = Double.valueOf(a1);
+       Double i2 = Double.valueOf(a2);
+
+        int b2 = i2.intValue();
+
+        if (b1>b2){
+            double i = b2;
+            System.out.println("Наибольшее число = "+b1);
             System.out.println("Тип double "+i);}
         else {
-            double j = b;
-            System.out.println("Наибольшее число = "+a2);
-            System.out.println("Тип double "+j);
+          //  double j = b1;
+            System.out.println("Наибольшее число = "+b2);
+            System.out.println("Тип double "+b1);
 
         }
         }
